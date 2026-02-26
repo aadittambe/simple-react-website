ed919dc31652512afbc3212c616aca1ab0410560
Aadit Tambe <aadit.tambe@gmail.com>
2026-02-25 19:48:08 -0500

    try

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Home from './Home';
-import Contact from './Contact';
-import About from './About';
-import Projects from './Projects'
-import { Routes, Route, Link } from 'react-router-dom'
-import NavBar from './NavBar';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Home";
+import Contact from "./Contact";
+import About from "./About";
+import Projects from "./Projects";
+import { Routes, Route, Link } from "react-router-dom";
+import NavBar from "./NavBar";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/about" element={<About />} />
-        <Route exact path="/projects" element={<Projects />} />
-        <Route exact path="/contact" element={<Contact />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </div>
   );
